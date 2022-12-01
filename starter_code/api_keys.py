07ef424c43af28e2b5a7ab97a3b46fc4f7604afb
ShahKarank <107441736+ShahKarank@users.noreply.github.com>
2022-11-30 18:50:38 -0600

    Update api_keys.py

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "525a6dcc2e551318a43a1f04a9721822"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDCLkmySirzrtjFogP6Zq0Z4ciUxmiqtRI"
